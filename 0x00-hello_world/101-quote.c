34dc802940ba28ed3684ee0fa0954924ee6155ef
Solomon <jaybartimeus@gmail.com>
2022-12-08 17:13:03 -0800

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using write function
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
