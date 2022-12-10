5672a8279d849cff2be769d45df6e9b5602b3d14
Solomon <jaybartimeus@gmail.com>
2022-12-09 17:49:06 -0800

    digits

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int a;
+	int n;
 
-	for (a = 0; a < 10; a++)
+	for (n = 0; n < 10; n++)
 	{
-		printf("%d", a);
+		printf("%d", n);
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
