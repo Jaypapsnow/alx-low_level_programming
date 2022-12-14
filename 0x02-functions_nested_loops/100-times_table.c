0a4c563972c08a295616cdc8e052112067ab7d4e
Solomon <jaybartimeus@gmail.com>
2022-12-14 15:39:43 -0800

    time tables for any number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_tables - print the time table of the input
+ * starting with 0
+ * @n: number to rpint time table.
+ */
+void print_times_table(int n)
+{
+	int num, mulp, prod;
+
+	if (n >=0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mulp = 1; mulp <= n; mulp++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mulp;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
